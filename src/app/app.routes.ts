55ae549ec10f6fab6dceae6e9f816f072b503ea2
KOZAKI Tsuneaki <kozaki.tsuneaki@gmail.com>
2023-07-05 06:37:11 +0900

    ルーティングの設定をする

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Route } from '@angular/router';
+import { ProductListComponent } from './product-list/product-list.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    component: ProductListComponent,
+    pathMatch: 'full',
+    title: 'Products | Booking App',
+  },
+];
